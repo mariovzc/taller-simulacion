5c9dcc0e566917d122b90d2009728cc7555ad3a8
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-03-12 18:24:32 -0500

    cambiando la forma para listar datos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
+// clase charts
 class Charts {
+  // generar datos de la aplicaccion
   generateData () {
     let arr = []
-    let counts = {};
+    let counts = {
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+      10: 0,
+      11: 0,
+      12: 0
+    }
     let obj = {
       data: {
           labels: [],
@@ -17,19 +31,43 @@ class Charts {
     times(500) (() => {
       arr.push(Math.ceil(Math.random().toFixed(2) * 100) / 100)
     })
-    arr.forEach((x) => { 
-      counts[x] = (counts[x] || 0)+1
+
+    arr.forEach((n) => { 
+      if (n < (1 / 36)) {
+        counts[2] += 1
+      } else if (n < (3 / 36)) {
+        counts[3] += 1
+      }else if (n < (6 / 36)) {
+        counts[4] += 1
+      }else if (n < (10 / 36)) {
+        counts[5] += 1
+      }else if (n < (15 / 36)) {
+        counts[6] += 1
+      }else if (n < (21 / 36)) {
+        counts[7] += 1
+      }else if (n < (26 / 36)) {
+        counts[8] += 1
+      }else if (n < (30 / 36)) {
+        counts[9] += 1
+      }else if (n < (33 / 36)) {
+        counts[10] += 1
+      }else if (n < (35 / 36)) {
+        counts[11] += 1
+      }else if (n < (36 / 36)) {
+        counts[12] += 1
+      }
     })
     this.datos = counts
     for (let key in counts ) {
       let value = counts[key]
-        let prom = value / 500
+        let prom = value
 
       obj.data.labels.push(key)
         obj.data.datasets[0].data.push(prom)
     }
     return obj.data;
   }
+  // genera la grafica
   GenerateChart () {
     const data = this.generateData()
     const options = {
@@ -54,10 +92,23 @@ class Charts {
       options: options
     })
   }
+  reloadTable () {
+    const data = this.datos
+    let html  = ""
+    for (let key in data) {
+      html += 
+      ` <tr>
+          <th scope="row" class="text-center">${key}</th>
+          <td class="text-center">${data[key]}</td>
+          <td class="text-center">${(data[key] / 500)}</td>
+        </tr>
+      `
+    }
+    document.getElementById("tabla").innerHTML = html
+  }
 }
 
-
-
+// Funcion para las iteraciones
 const times = n => f => {
   let iter = i => {
     if (i === n) return
@@ -67,9 +118,15 @@ const times = n => f => {
   return iter (0)
 }
 
+// ejecutando la clase charts 
 const charts = new Charts
-charts.GenerateChart()
 
+//cargando la informacion
+charts.GenerateChart()
+charts.reloadTable()
+
+// generar valores aleatorios de nuevo
 document.getElementById('generar').onclick = () => {
-  charts.GenerateChart(val)
+  charts.GenerateChart()
+  charts.reloadTable()
 }
